524fb14b8a7995bf27391b2cf68f7996bb251ec6
David Hsieh <61106644+davidyen-888@users.noreply.github.com>
2021-08-07 17:15:22 +0800

    Add deuce feature

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ let winningScore = 3;
 
 p1.button.addEventListener('click', () => {
     updateScore(p1, p2);
+    deuce(p1, p2);
 })
 
 p2.button.addEventListener('click', () => {
     updateScore(p2, p1);
+    deuce(p2, p1);
 })
 
 selectScore.addEventListener('change', () => {
@@ -42,6 +44,17 @@ function updateScore(player, opponent) {
 
 }
 
+// check condition of winning by 2 points
+function deuce(player, opponent) {
+    if (player.score === opponent.score && player.score === winningScore - 1) {
+        winningScore += 1;
+        selectScore.selectedOptions[0].value = winningScore;
+        selectScore.classList.add('deuce');
+        selectScore.selectedOptions[0].textContent = winningScore;
+    }
+}
+
+
 function reset() {
     for (let p of [p1, p2]) {
         p.score = 0;
@@ -49,4 +62,9 @@ function reset() {
         p.display.classList.remove('has-text-success', 'has-text-danger');
         p.button.disabled = false;
     }
+    selectScore.classList.remove('deuce');
+    for (let i=0; i <=6; i++){
+        selectScore[i].value = 3 + i;
+        selectScore[i].innerText = 3 + i;
+     }
 }
