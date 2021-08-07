048078fcc7c3ee2c1604143490b4f52be7c6eb8d
David Hsieh <61106644+davidyen-888@users.noreply.github.com>
2021-08-07 17:48:06 +0800

    Score resizing

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ resetButton.addEventListener('click', reset);
 
 function updateScore(player, opponent) {
     player.score += 1;
+    resizeScore(player, opponent);
     if (player.score === winningScore) {
         isGameOver = true;
         player.display.classList.add('has-text-success');
@@ -41,7 +42,7 @@ function updateScore(player, opponent) {
         opponent.button.disabled = true;
     }
     player.display.textContent = player.score;
-
+    
 }
 
 // check condition of winning by 2 points
@@ -54,6 +55,19 @@ function deuce(player, opponent) {
     }
 }
 
+// resizing score while in the game
+function resizeScore(player, opponent) {
+    if (player.score > opponent.score) {
+        player.display.classList.add('winning');
+        opponent.display.classList.remove('winning');
+    } else if (opponent.score > player.score) {
+        opponent.display.classList.add('winning');
+        player.display.classList.remove('winning');
+    } else {
+        player.display.classList.remove('winning');
+        opponent.display.classList.remove('winning');
+    }
+}
 
 function reset() {
     for (let p of [p1, p2]) {
@@ -63,8 +77,8 @@ function reset() {
         p.button.disabled = false;
     }
     selectScore.classList.remove('deuce');
-    for (let i=0; i <=6; i++){
+    for (let i = 0; i <= 6; i++) {
         selectScore[i].value = 3 + i;
         selectScore[i].innerText = 3 + i;
-     }
+    }
 }
